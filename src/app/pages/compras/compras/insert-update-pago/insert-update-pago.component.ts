009e9d89dcc5296549c049ce447d04ab47aa1c27
Paul-Acevedo <paulacevedo175@gmail.com>
2023-04-22 12:35:58 -0600

    correcciones

--- src/app/pages/compras/compras/insert-update-pago/insert-update-pago.component.ts
+++ src/app/pages/compras/compras/insert-update-pago/insert-update-pago.component.ts
@@ -6,6 +6,8 @@ import { ProveedoresPackageService } from '../../proveedores/proveedores-package
 import { Observable } from 'rxjs';
 import { startWith, map } from 'rxjs/operators';
 import { Confirm } from 'notiflix';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 
 @Component({
   selector: 'app-insert-update-pago',
@@ -58,7 +60,7 @@ export class InsertUpdatePagoComponent {
     // crea usuario
     let datos = this._service.register.value;
     let params = {
-      codproveedor: this._service.pago.value.COD_PROVEEDOR,
+      codproveedor: this._service.pago.value.COD_PERSONA.COD_PERSONA,
       total: this._service.total,
       productos: this._service.productos,
       user: localStorage.getItem('user'),
@@ -77,26 +79,284 @@ export class InsertUpdatePagoComponent {
           idusuario: 1,
           tabla: 'COMPRAS',
         };
-        this._service.productos = [];
+       
 
 
         Confirm.show(
-          'Notiflix Confirm',
-          'Do you agree with me?',
-          'Yes',
+          'Confirmar',
+          'Desea imprimir factura?',
+          'Si',
           'No',
           () => {
-          alert('Thank you.');
+         
+           let datos:any = []
+            console.log(this._service.productos);
+
+            for (var i = 0; i < this._service.productos.length; i++) {
+             datos.push([this._service.productos[i].producto,this._service.productos[i].precio,this._service.productos[i].cantidad])
+            }
+              const doc = new jsPDF();
+          
+              autoTable(doc, {
+                body: [
+                  [
+                    {
+                      content: 
+
+                      'AGROCOMERCIAL "La libertad"' +
+                      '\nCESAR A. ANDINO R.T.N 03061953000851' +
+                      '\nBo. La flor,La libertad,Comayagua' +
+                      '\n Tel: 2724-0568 - 97809709'
+                      ,
+                      styles: {
+                        halign: 'center',
+                        fontSize: 20,
+                        // textColor: '#ffffff',
+                      },
+                    },
+                    // {
+                    //   content: 'Factura',
+                    //   styles: {
+                    //     halign: 'center',
+                    //     fontSize: 20,
+                    //     textColor: '#ffffff',
+                    //   },
+                    // },
+                  ],
+                ],
+                theme: 'plain',
+                // styles: {
+                //   fillColor: '#fff',
+                // },
+              });
+          
+              autoTable(doc, {
+                body: [
+                  [
+                    {
+                      content:
+                        `CAI: #INV0001` +
+                        `\nFACTURA SAR: 123456` +
+                        `\nFECHA: ${new Date()}` ,
+                      styles: {
+                        halign: 'right',
+                      },
+                    },
+                  ],
+                ],
+                theme: 'plain',
+              });
+          
+              autoTable(doc, {
+                body: [
+                  [
+                    {
+                      content:
+                        'Codigo factura' +
+                        '\nVenta:efectivo' +
+                        '\nNombre cliente' ,
+                        // '\nBilling Address line 2' +
+                        // '\nZip code - City' +
+                        // '\nCountry',
+                      styles: {
+                        halign: 'left',
+                      },
+                    },
+                    // {
+                    //   content:
+                    //     'Shipping address:' +
+                    //     '\nJohn Doe' +
+                    //     '\nShipping Address line 1' +
+                    //     '\nShipping Address line 2' +
+                    //     '\nZip code - City' +
+                    //     '\nCountry',
+                    //   styles: {
+                    //     halign: 'left',
+                    //   },
+                    // },
+                    // {
+                    //   content:
+                    //     'From:' +
+                    //     '\nCompany name' +
+                    //     '\nShipping Address line 1' +
+                    //     '\nShipping Address line 2' +
+                    //     '\nZip code - City' +
+                    //     '\nCountry',
+                    //   styles: {
+                    //     halign: 'right',
+                    //   },
+                    // },
+                  ],
+                ],
+                theme: 'plain',
+              });
+          
+              // autoTable(doc, {
+              //   body: [
+              //     [
+              //       {
+              //         content: 'Amount due:',
+              //         styles: {
+              //           halign: 'right',
+              //           fontSize: 14,
+              //         },
+              //       },
+              //     ],
+              //     [
+              //       {
+              //         content: '$4000',
+              //         styles: {
+              //           halign: 'right',
+              //           fontSize: 20,
+              //           textColor: '#3366ff',
+              //         },
+              //       },
+              //     ],
+              //     [
+              //       {
+              //         content: 'Due date: 2022-02-01',
+              //         styles: {
+              //           halign: 'right',
+              //         },
+              //       },
+              //     ],
+              //   ],
+              //   theme: 'plain',
+              // });
+          
+              autoTable(doc, {
+                body: [
+                  [
+                    {
+                      content: 'Productos',
+                      styles: {
+                        halign: 'left',
+                        fontSize: 14,
+                      },
+                    },
+                  ],
+                ],
+                theme: 'plain',
+              });
+          
+              autoTable(doc, {
+                head: [['Producto', 'Precio', 'Cantidad']],
+                  
+                body: datos,
+               
+                // [
+                //   ['Producto', 'Category', '2', '$450', '$1000'],
+                //   ['Producto', 'Category', '2', '$450', '$1000'],
+                //   ['Producto', 'Category', '2', '$450', '$1000'],
+                //   ['Product0', 'Category', '2', '$450', '$1000'],
+                // ],
+                theme: 'striped',
+                headStyles: {
+                  fillColor: '#343a40',
+                },
+              });
+          
+              autoTable(doc, {
+                body: [
+                  [
+                    {
+                      content: 'Sub total:',
+                      styles: {
+                        halign: 'right',
+                      },
+                    },
+                    {
+                      content: 'Lps. '+ this._service.total,
+                      styles: {
+                        halign: 'right',
+                      },
+                    },
+                  ],
+
+
+                  [
+                    {
+                      content: 'Total:',
+                      styles: {
+                        halign: 'right',
+                      },
+                    },
+                    {
+                      content: 'Lps. '+this._service.total,
+                      styles: {
+                        halign: 'right',
+                      },
+                    },
+                  ],
+                  // [
+                  //   {
+                  //     content: 'Total amount:',
+                  //     styles: {
+                  //       halign: 'right',
+                  //     },
+                  //   },
+                  //   {
+                  //     content: '$4000',
+                  //     styles: {
+                  //       halign: 'right',
+                  //     },
+                  //   },
+                  // ],
+                ],
+                theme: 'plain',
+              });
+          
+              autoTable(doc, {
+                body: [
+                  [
+                    {
+                      content: 'Terminos y notas',
+                      styles: {
+                        halign: 'left',
+                        fontSize: 14,
+                      },
+                    },
+                  ],
+                  [
+                    {
+                      content:
+                        'Fecha limite de emision' +
+                        'Rango desde hasta' +
+                        'Original cliente',
+                      styles: {
+                        halign: 'left',
+                      },
+                    },
+                  ],
+                ],
+                theme: 'plain',
+              });
+          
+              autoTable(doc, {
+                body: [
+                  [
+                    {
+                      content: '!GRACIAS POR SU COMPRA!',
+                      styles: {
+                        halign: 'center',
+                      },
+                    },
+                  ],
+                ],
+                theme: 'plain',
+              });
+          
+             doc.save('factura');
+            
           },
           () => {
-          alert('If you say so...');
-          },
-          {
+          
           },
           );
 
       }
       this._service.mostrar();
+     // this._service.productos = [];
       ///  this.cerrarmodal();
     });
   }

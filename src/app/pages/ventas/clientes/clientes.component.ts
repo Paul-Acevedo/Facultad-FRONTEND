bd3b8e954712d9afb61f13fa6eb72d9431334466
= <=>
2023-08-26 09:43:11 -0600

    correcciones

--- src/app/pages/ventas/clientes/clientes.component.ts
+++ src/app/pages/ventas/clientes/clientes.component.ts
@@ -135,7 +135,7 @@ export class ClientesComponent implements OnInit {
   </div><br>`;
 
     printJS({
-      printable: 'reporte',
+      printable: 'reporte2',
       type: 'html',
       header: rawHTML,
       css: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
